5431f7b086101e7a04a562afb9276ea9e0fe62bb
eirikraha <eirikhauge@hotmail.com>
2018-12-07 16:39:10 +0100

    Scaled X

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import copy as cp
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.utils import np_utils, to_categorical
@@ -33,10 +34,48 @@ for i in range(0, len(keys) - 1):
 	X = np.c_[X, data_dict[keys[i]]]
 
 Y = data_dict[keys[-1]]
+ones_index = []
+zeros_index = []
+
+for i in enumerate(Y):
+	if i[1] == 1:
+		ones_index.append(i[0])
+	else:
+		zeros_index.append(i[0])
+
+
+#print (len(zeros_index)/(len(ones_index)))
+
+#Brute force, because the smart way didn't work
+X_ones = np.zeros((len(ones_index), len(X[0])))
+Y_ones = np.ones(len(ones_index))
+for i in enumerate(ones_index):
+	X_ones[i[0]] = X[i[1]]
+
+#print (X_ones.shape, X.shape, X.shape[0] + X_ones.shape[0]*3, len(Y))
+
+# X = np.r_[X, X_ones, X_ones, X_ones] #, X_ones[:int(np.floor(X_ones.shape[0]*0.52))]]
+# Y = np.r_[Y, Y_ones, Y_ones, Y_ones] #, Y_ones[:int(np.floor(Y_ones.shape[0]*0.52))]]
+
+# Y_counter = 0
+# for i in enumerate(Y):
+# 	if i[1] == 1:
+# 		Y_counter += 1
+
+# print ('Ratio of ones to zeros:', Y_counter/float(len(Y)))
+
+# print (X.shape, Y.shape)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3)
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 if (sys.argv[1] == "b") or (sys.argv[1] == 'All'):
 
-	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3)
+	
 
 	LogReg = LogisticRegression()
 	LogReg.fit(X_train, Y_train)
@@ -53,14 +92,12 @@ if (sys.argv[1] == "b") or (sys.argv[1] == 'All'):
 	summer = 0
 	for i in range(0, len(pred)):
 		if Y_test[i] == 1 and pred[i] == Y_test[i]:
-			print (pred[i], Y_test[i])
 			summer += 1
 
-	print (summer/float(len(pred)))
+	print (summer/float(len(pred)), ' If there is bad balane, this would be 0')
 
 if (sys.argv[1] == "c") or (sys.argv[1] == 'All'):
 
-	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3)
 	Y_train = to_categorical(Y_train)
 
 
@@ -81,18 +118,18 @@ if (sys.argv[1] == "c") or (sys.argv[1] == 'All'):
 
 	
 
-	sgd = SGD(lr=0.001, decay=1e-7, momentum=.9)
+	sgd = SGD(lr=0.01, decay=1e-7, momentum=.9)
 	model.compile(loss='categorical_crossentropy', 
 	              optimizer=sgd, 
 	              metrics=['accuracy'])
 
 	model.fit(X_train, Y_train, 
-	          epochs=10, # Keras 2: former nb_epoch has been renamed to epochs
-	          batch_size=32, 
+	          epochs=50, # Keras 2: former nb_epoch has been renamed to epochs
+	          batch_size=128, 
 	          verbose=1) 
 
 	Y_train_pred = model.predict_classes(X_train, verbose=0)
-	print('First 3 predictions: ', Y_train_pred[:3])
+	print('Average predictions: ', np.mean(Y_train_pred))
 
 	train_acc = np.sum(Y_train == Y_train_pred, axis=0) / X_train.shape[0]
 	print('Training accuracy: %.2f%%' % (train_acc * 100))
@@ -101,10 +138,5 @@ if (sys.argv[1] == "c") or (sys.argv[1] == 'All'):
 	test_acc = np.sum(Y_test == Y_test_pred, axis=0) / X_test.shape[0]
 	print('Test accuracy: %.2f%%' % (test_acc * 100))
 
-	if sys.argv[1] == 'All':
-		print(max(pred), max(Y_train_pred))
-		for i in range(0, len(pred)):
-			if pred[i] != Y_train_pred[i]:
-				print (pred[i], Y_train_pred[i])
 else:
 	print ('Write in task you wish to perform')
