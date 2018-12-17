b63b936b6b43b1c1d01eaadc802f4b62d842755e
eirikraha <eirikhauge@hotmail.com>
2018-12-18 00:11:39 +0100

    Added colorplot

--- src/main.py
+++ src/main.py
@@ -17,6 +17,39 @@ from keras import regularizers
 
 import functions as fcn
 
+def ColorPlotter(x, y, z, method, xlabel, ylabel, cbartitle, mini=0, maxi=1,
+	 fs1 = 20, fs2 = 20, fs3 = 20, show = False):
+	#Creates colorplots
+
+	fig, ax = plt.subplots(figsize = (18,9))
+
+	yheaders = ['%1.2e' %i  for i in y]
+	xheaders = ['%1.2f' %i for i in x]
+
+	print (z)
+	heatmap = ax.pcolor(z, edgecolors = "k", linewidth = 2, vmin = mini, vmax = maxi)
+	cbar = plt.colorbar(heatmap, ax = ax)
+	cbar.ax.tick_params(labelsize= fs3) 
+	cbar.ax.set_title(cbartitle, fontsize = fs2)
+
+
+	ax.set_title(method, fontsize = fs1)
+	ax.set_xticks(np.arange(z.shape[1]) +.5, minor = False)
+	ax.set_yticks(np.arange(z.shape[0]) +.5, minor = False)
+
+	ax.set_xticklabels(xheaders,rotation=90, fontsize = fs3)
+	ax.set_yticklabels(yheaders, fontsize = fs3)
+
+	ax.set_xlabel(xlabel, fontsize = fs2)
+	ax.set_ylabel(ylabel, fontsize = fs2)
+
+	plt.tight_layout()
+
+	plt.savefig('../figures/%s-%s-%s-%s-mini%1.2f-maxi%1.2f.png' %(method, 
+				ylabel, yheaders[-1],cbartitle, mini, maxi))
+	if show:
+		plt.show()
+
 
 def XLSread(filename):
 	data = pd.read_excel(filename)
@@ -131,134 +164,171 @@ if (sys.argv[1] == "b") or (sys.argv[1] == 'All'):
 
 if (sys.argv[1] == "c") or (sys.argv[1] == 'All'):
 
+	Y_train_temp = Y_train
 	Y_train = to_categorical(Y_train)
 
 	net_type = "Standard"
 
-	filename = '../benchmarks/mom_NN%s_data.txt' % (net_type)
+	# filename_save = '../benchmarks/mom_NN%s_data.txt' % (net_type)
 
-	f = open(filename, 'w')
-	f.write('### Start of neural net file ###')
-	f.close()
-
-
-	epochs_array = [i*20 for i in range(3, 10)]
-	nb = [2**i for i in range(6, 12)]
-	lr = [0.1, 0.01, 0.001, 0.0001]
-	decay = [1e-5, 1e-6, 1e-7, 1e-8, 1e-9]
-	momentum = [.7, .8, .9, 1.0, 1.1, 1.2]
-	lmbs = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 10, 100]
+	# f = open(filename_save, 'w')
+	# f.write('### Start of neural net file ### \n\n')
+	# f.close()
 
 
-
-	for lmb in lmbs:
-
-		if (len(sys.argv) > 2) and (sys.argv[2] == "Deep"):
-
-			net_type = "Deep"
-
-			model = Sequential()
-			model.add(Dense(60, input_shape = (X_train.shape[1], )), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(10), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(2), kernel_regularizer=regularizers.l2(lmb))
-			model.add(Activation('softmax'))
-
-		elif (len(sys.argv) > 2) and (sys.argv[2] == "Wide"):
-
-			net_type = "Wide"
-
-			model = Sequential()
-			model.add(Dense(512, input_shape = (X_train.shape[1], )))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(512))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(2))
-			model.add(Activation('softmax'))
-
-		else:
-			model = Sequential()
-			model.add(Dense(64, input_shape = (X_train.shape[1], ), kernel_regularizer=regularizers.l2(lmb)))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(32, kernel_regularizer=regularizers.l2(lmb)))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(32, kernel_regularizer=regularizers.l2(lmb)))
-			model.add(Activation('relu'))
-			model.add(Dropout(0.2))
-			model.add(Dense(2, kernel_regularizer=regularizers.l2(lmb)))
-			model.add(Activation('softmax'))
+	epochs_array = [50] #[i*20 for i in range(3, 10)]
+	nbs = [512]  #[2**i for i in range(6, 12)]
+	lrs = [0.01] #[0.1, 0.01, 0.001, 0.0001]
+	decays = [1e-7, 1e-8] #[1e-5, 1e-6, 1e-7, 1e-8, 1e-9]
+	momentums = [.9]# , 1.0, 1.1, 1.2]
+	lmbs = [1e-5] #[1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 10, 100]
 
 
+	x_list = []
+	y_list = []
+	z_list = np.zeros((2, 1))
+	z2_list = np.zeros((2, 1))
 
-		sgd = SGD(lr=0.01, decay=1e-8, momentum=1e-8)
-		model.compile(loss='categorical_crossentropy', 
-	              optimizer=sgd, 
-	              metrics=['accuracy'])
+	print (z_list)
 
-		model.fit(X_train, Y_train, 
-		          epochs=140, # Keras 2: former nb_epoch has been renamed to epochs
-		          batch_size=512, 
-		          verbose=1) 
+	for x in momentums:
+		x_list.append(x)
+		x_title = 'Momentum'
+		momentum = x
+		lmb = lmbs[0]
+		epochs = epochs_array[0]
+		nb = nbs[0]
+		lr = lrs[0]
+		cbartitle = 'Accuracy'	
+		
+		for y in decays:
+
+			y_list.append(y)
+			y_title = 'Decay'
+			decay = y
+
+			print(y, y_list)
 
 
-		f = open(filename, 'a')
+			if (len(sys.argv) > 2) and (sys.argv[2] == "Deep"):
 
-		print ('Type: %s' %net_type)
-		print ('Epochs: %d' %140)
-		print ('Batch size: %d' %512)
-		print ('Learning rate: %1.2e' %0.01)
-		print ('Decay: %1.2e' %1e-8)
-		print ('Momentum: %1.2f' %1e-8)
-		print ('Lambda: %1.2e' %lmb)
-		Y_train_pred = model.predict_classes(X_train, verbose=0)
-		print('Average predictions: ', np.mean(Y_train_pred))
+				net_type = "Deep"
 
-		train_acc = np.sum(Y_train == Y_train_pred, axis=0) / X_train.shape[0]
-		print('Training accuracy: %.2f%%' % (train_acc * 100))
+				model = Sequential()
+				model.add(Dense(60, input_shape = (X_train.shape[1], )), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(40), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(10), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(2), kernel_regularizer=regularizers.l2(lmb))
+				model.add(Activation('softmax'))
 
-		Y_test_pred = model.predict_classes(X_test, verbose=0)
-		test_acc = np.sum(Y_test == Y_test_pred, axis=0) / X_test.shape[0]
-		print('Test accuracy: %.2f%%' % (test_acc * 100))
+			elif (len(sys.argv) > 2) and (sys.argv[2] == "Wide"):
 
-		f.write('Epochs: %d \n' %140)
-		f.write('Batch size: %d \n' %512)
-		f.write('Learning rate: %1.2e \n' %0.01)
-		f.write('Decay: %1.2e \n' %1e-8)
-		f.write('Momentum: %1.2f \n' %1e-8)
-		f.write('Lambda: %1.2e' %lmb)
-		f.write('Average predictions: %.2f%%  \n' % np.mean(Y_train_pred))
-		f.write('Training accuracy: %.2f%%  \n' % (train_acc * 100))
-		f.write('Test accuracy: %.2f%%  \n' % (test_acc * 100))
+				net_type = "Wide"
 
-		f.close()
+				model = Sequential()
+				model.add(Dense(512, input_shape = (X_train.shape[1], )))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(512))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(2))
+				model.add(Activation('softmax'))
+
+			else:
+				model = Sequential()
+				model.add(Dense(64, input_shape = (X_train.shape[1], ), kernel_regularizer=regularizers.l2(lmb)))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(32, kernel_regularizer=regularizers.l2(lmb)))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(32, kernel_regularizer=regularizers.l2(lmb)))
+				model.add(Activation('relu'))
+				model.add(Dropout(0.2))
+				model.add(Dense(2, kernel_regularizer=regularizers.l2(lmb)))
+				model.add(Activation('softmax'))
+
+
+
+			sgd = SGD(lr=lr, decay=decay, momentum=momentum)
+			model.compile(loss='categorical_crossentropy', 
+		              optimizer=sgd, 
+		              metrics=['accuracy'])
+
+			model.fit(X_train, Y_train, 
+			          epochs=epochs, # Keras 2: former nb_epoch has been renamed to epochs
+			          batch_size=nb, 
+			          verbose=0) 
+
+
+			f = open(filename, 'a')
+
+			print ('Type: %s' %net_type)
+			print ('Epochs: %d' %epochs)
+			print ('Batch size: %d' %nb)
+			print ('Learning rate: %1.2e' %lr)
+			print ('Decay: %1.2e' %decay)
+			print ('Momentum: %1.2f' %momentum)
+			print ('Lambda: %1.2e' %lmb)
+			
+			Y_train_pred = model.predict_classes(X_train, verbose=0)
+			print('Average predictions: ', np.mean(Y_train_pred))
+
+			train_acc = np.sum(Y_train_temp == Y_train_pred, axis=0) / X_train.shape[0]
+			
+			print('Training accuracy: %.2f%%' % (train_acc * 100))
+
+			Y_test_pred = model.predict_classes(X_test, verbose=0)
+			test_acc = np.sum(Y_test == Y_test_pred, axis=0) / X_test.shape[0]
+
+			print('Test accuracy: %.2f%%' % (test_acc * 100))
+
+			print([len(y_list) - 1, len(x_list) - 1])  #To make col x row work!
+			print(z_list.shape)
+
+			z_list[len(y_list) - 1, len(x_list) - 1] = train_acc
+			z2_list[len(y_list) - 1, len(x_list) - 1] = test_acc
+
+			# f.write('Epochs: %d \n' %epochs)
+			# f.write('Batch size: %d \n' %nb)
+			# f.write('Learning rate: %1.2e \n' %lr)
+			# f.write('Decay: %1.2e \n' %decay)
+			# f.write('Momentum: %1.2f \n' %momentum)
+			# f.write('Lambda: %1.2e\n' %lmb)
+			# f.write('Average predictions: %.2f%%  \n' % np.mean(Y_train_pred))
+			# f.write('Training accuracy: %.2f%%  \n' % (train_acc * 100))
+			# f.write('Test accuracy: %.2f%%  \n' % (test_acc * 100))
+
+			# f.write('\n\n')
+			# f.close()
+
+	ColorPlotter(x_list, y_list, z_list, net_type, x_title, y_title, cbartitle, 
+		mini = np.min(z_list), maxi = np.max(z_list), show = True)
     
 if (sys.argv[1] == "d") or (sys.argv[1] == 'All'):
     
